7aa02faeb36a895c1096a1c24ff12ba9d00a7b59
Mesar Hameed <mesar.hameed@gmail.com>
2021-02-21 21:41:31 +0000

    use sh, rather than ash or bash

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env ash
+#!/usr/bin/env sh
 # Load variables into the environment, and start waitress
-source $(dirname $(readlink -f $0))/setenv.sh
+. $(dirname $(readlink -f $0))/setenv.sh
 $(dirname $(readlink -f $0))/mywaitress.py
