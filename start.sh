72d777e18b11357a7b25cb702c7b0c72161735ed
Mesar Hameed <mesar.hameed@gmail.com>
2021-02-28 11:08:15 +0000

    activate virtualenv as part of the start.sh script

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+# activate virtualenv
+. $(dirname $(readlink -f $0))/.venv/bin/activate
 # Load variables into the environment, and start waitress
 . $(dirname $(readlink -f $0))/setenv.sh
 $(dirname $(readlink -f $0))/mywaitress.py
