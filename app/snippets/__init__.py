3659a8784ca0cf0a701afce68441abe4c0f4684e
Mesar Hameed <mesar.hameed@gmail.com>
2021-03-20 11:55:02 +0000

    fix bug: base64 change method

--- app/snippets/__init__.py
+++ app/snippets/__init__.py
@@ -122,7 +122,7 @@ class SnippetsView(FlaskView):
         if entry.token != token:
             return make_response(jsonify(status='token does not match, you are not authorized'), 403)
         entry.status = request.json['status']
-        audio = base64.decodestring(request.json['audio'])
+        audio = base64.standard_b64decode(request.json['audio'])
         try:
             os.mkdir(os.path.join(os.getcwd(), 'audio'))
         except Exception:
