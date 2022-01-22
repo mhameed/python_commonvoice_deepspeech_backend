cfce52c4063394748b8fd3c61ba7e121854c1d91
Mesar Hameed <mesar.hameed@gmail.com>
2022-01-22 14:49:20 +0000

    make use of trans logger to get an access log so that we can see what urls are being requested.

--- mywaitress.py
+++ mywaitress.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from os import environ as env
 from waitress import serve
+from paste.translogger import TransLogger
 import logging
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.INFO)
 
 import app
-serve(app.create_app(), listen='*:'+env['serveport'], trusted_proxy='10.1.3.83', url_prefix=env['url_prefix'])
+serve(TransLogger(app.create_app(), setup_console_handler=False), listen='*:'+env['serveport'], trusted_proxy='10.1.3.83', url_prefix=env['url_prefix'])
