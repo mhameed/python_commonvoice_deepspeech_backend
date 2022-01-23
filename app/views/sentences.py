8f7fc3d516d6ada74be5e21fe9c3580b8f73049f
Mesar Hameed <mesar.hameed@gmail.com>
2022-01-23 17:07:42 +0000

    add logging to sentences

--- app/views/sentences.py
+++ app/views/sentences.py
@@ -7,6 +7,8 @@ from sqlalchemy.exc import IntegrityError
 from  sqlalchemy.sql.expression import func
 from app import db
 from ..models import Sentence
+import logging
+logger = logging.getLogger('cv.sentences')
 
 bp = Blueprint('sentences', __name__, url_prefix='/sentences')
 
@@ -21,13 +23,16 @@ def get():
         count = 1
     elif count > 100:
         count = 100
+    logger.debug(f"get: Received a request count:{count}")
     resp = []
     for s in Sentence.query.order_by(func.random()):
         if len(s.clips) > 1: continue
         resp.append(s.to_dict())
         if len(resp) == count: break
     if resp is []:
+        logger.debug(f"get: returning {resp}")
         return make_response(jsonify(status='No result found'), 404)
+    logger.debug(f"get: returning {resp}")
     return jsonify(resp)
 
 @bp.route('', methods=['POST'])
