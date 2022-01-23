48206a4707c275cf9867e3f15bb340cf7209a525
Mesar Hameed <mesar.hameed@gmail.com>
2022-01-23 17:22:10 +0000

    mywaitress: logging

--- mywaitress.py
+++ mywaitress.py
@@ -3,8 +3,8 @@ from os import environ as env
 from waitress import serve
 from paste.translogger import TransLogger
 import logging
-logger = logging.getLogger('waitress')
-logger.setLevel(logging.INFO)
-
+logger = logging.getLogger('cv')
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(filename='cv.log', encoding='utf-8', level=logging.DEBUG)
 import app
 serve(TransLogger(app.create_app(), setup_console_handler=False), listen='*:'+env['serveport'], trusted_proxy='10.1.3.83', url_prefix=env['url_prefix'])
