6852e353280ce12582df7cd4109a846b7c8a24c3
Mesar Hameed <mesar.hameed@gmail.com>
2022-01-23 21:55:07 +0000

    resources: get the audio from db rather than from filesystem

--- app/views/resources.py
+++ app/views/resources.py
@@ -2,7 +2,7 @@ import uuid
 import json
 import sys
 import os
-from flask import Blueprint, g, jsonify, abort, request, make_response, url_for, render_template, Response, send_from_directory, send_file
+from flask import Blueprint, g, jsonify, abort, request, make_response, url_for, render_template, Response
 from sqlalchemy.exc import IntegrityError
 from app import db
 from ..models import Clip
@@ -14,9 +14,11 @@ bp = Blueprint('resources', __name__, url_prefix='/resources')
 @bp.route('/<id>', methods=['GET'])
 def get(id):
     logger.debug(f"get: Received a request with id:{id}")
-    try:
-        fname = f"{id}.mp3"
-        return send_from_directory(os.path.join(os.getcwd(), 'audio'), fname, as_attachment=False, cache_timeout=30)
-    except Exception as e:
-        logger.debug(f"get: Could not find resource with id:{id}")
-        return make_response(jsonify(status='File not found.'), 404)
+    if id.startswith('u-'):
+        u = Unrecognized.query.filter(Unrecognized.id==id).first()
+        return Response(u.data, mimetype='audio/ogg')
+    elif id.startswith('a-'):
+        c = Clip.query.filter(Clip.id==id).first()
+        return Response(c.data, mimetype='audio/ogg')
+    logger.debug(f"get: Could not find resource with id:{id}")
+    return make_response(jsonify(status='File not found.'), 404)
