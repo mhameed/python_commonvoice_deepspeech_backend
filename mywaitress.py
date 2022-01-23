f66b6476a44418678c48f9f33a6d50d46a5915ae
Mesar Hameed <mesar.hameed@gmail.com>
2022-01-23 21:58:14 +0000

    reordering + set waitress logging level

--- mywaitress.py
+++ mywaitress.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
-from os import environ as env
-from waitress import serve
-from paste.translogger import TransLogger
+import app
 import logging
+from os import environ as env
+from paste.translogger import TransLogger
+from waitress import serve
+
 logger = logging.getLogger('cv')
 logger.setLevel(logging.DEBUG)
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.DEBUG)
+
 logging.basicConfig(filename='cv.log', encoding='utf-8', level=logging.DEBUG)
-import app
+
 serve(TransLogger(app.create_app(), setup_console_handler=False), listen='*:'+env['serveport'], trusted_proxy='10.1.3.83', url_prefix=env['url_prefix'])
