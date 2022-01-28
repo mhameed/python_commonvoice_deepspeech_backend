b4d277348e5b3aa147766fa09b3fa6136bfc9690
Mesar Hameed <mesar.hameed@gmail.com>
2022-01-28 09:25:17 +0000

    add metrics to resources view

--- app/views/resources.py
+++ app/views/resources.py
@@ -4,15 +4,18 @@ import sys
 import os
 from flask import Blueprint, g, jsonify, abort, request, make_response, url_for, render_template, Response
 from sqlalchemy.exc import IntegrityError
-from app import db
+from app import db, metrics
 from ..models import Clip
 import logging
 logger = logging.getLogger('cv.resources')
 
 bp = Blueprint('resources', __name__, url_prefix='/resources')
-
+metrics['cv_calls'].labels(method='get', endpoint='/{id}', view='resources')
 @bp.route('/<id>', methods=['GET'])
 def get(id):
+    # two metrics, one that includes the id, and one that has the template of the id so that we can count how many times resources itself has been called regardless of id
+    metrics['cv_calls'].labels(method='get', endpoint='/{id}', view='resources').inc()
+    metrics['cv_calls'].labels(method='get', endpoint=f'/{id}', view='resources').inc()
     logger.debug(f"get: Received a request with id:{id}")
     if id.startswith('u-'):
         u = Unrecognized.query.filter(Unrecognized.id==id).first()
