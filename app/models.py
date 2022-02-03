9a528dc338e1dc154663d05155f359a650dd3a89
Mesar Hameed <mesar.hameed@gmail.com>
2022-02-03 14:49:34 +0000

    optional source field for sentences, incase we want to track where the sentence came from

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class Sentence(db.Model):
     language = db.Column(db.String(7), nullable=False, default='en')
     user = db.Column(db.String(20), nullable=False)
     text = db.Column(db.String(255), nullable=False, unique=True)
+    source = db.Column(db.String(100))
     clips = relationship("Clip", back_populates="sentence")
 
     def __init__(self, *args, **kwargs):
@@ -21,15 +22,16 @@ class Sentence(db.Model):
         self.text = kwargs.get('text')
         self.language = kwargs.get('language')
         self.user = kwargs.get('user')
+        self.source = kwargs.get('source', '')
 
     def __repr__(self):
         return self.__str__()
 
     def __str__(self):
-        return "<sentence('%s', '%s', '%s', '%s')>" % (self.id, self.language, self.user, self.text)
+        return "<sentence('%s', '%s', '%s', '%s', '%s')>" % (self.id, self.language, self.user, self.text, self.source)
 
     def to_dict(self):
-        return {'id':self.id, 'language':self.language, 'text':self.text, 'user':self.user}
+        return {'id':self.id, 'language':self.language, 'text':self.text, 'source':self.source, 'user':self.user}
 
     def delete(self):
         db.session.delete(self)
