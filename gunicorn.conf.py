8eed1ecf74681a48b0dfbaac87d25dae42362a4a
Mesar Hameed <mesar.hameed@gmail.com>
2022-02-17 11:48:36 +0000

    gunicorn config file to include prometheus metric exporter

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,5 @@
+from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+
 bind = "10.1.2.100:8000"
 forwarded_allow_ips = "10.1.95.117"
 proxy_protocol = True
@@ -5,3 +7,11 @@ proxy_allow_ips = "10.1.95.117"
 wsgi_app = 'speech_api:app'
 accesslog = "logs/access.log"
 errorlog = "logs/error.log"
+metrics_port = 9000
+
+def when_ready(server):
+    GunicornPrometheusMetrics.start_http_server_when_ready(metrics_port)
+
+
+def child_exit(server, worker):
+    GunicornPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
